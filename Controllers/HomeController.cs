59fd03112ca60e2c24893c852f3b865bbfc85965
LenovO <LenovO@DESKTOP-HJIH5Q6>
2022-11-10 20:12:23 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using DataTablePlugin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataTablePlugin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        employeeDBEntities db = new employeeDBEntities(); 
+        public ActionResult Index()
+        {
+            var data = db.Employees.ToList();
+            return View(data);
+        }
+
+        public ActionResult MyDataTable()
+        {
+            return View();
+        }
+
+        public ActionResult EmpData()
+        {
+            var data = db.Employees.ToList();
+            return View(data);
+        }
+    }
+}
